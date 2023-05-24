aa93eb3c356867e21497931a43b7f9107806b5f2
Erik van der Bas <erik@basedonline.nl>
2023-05-24 10:14:09 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-const path = require('path');
-const fs = require('fs');
-const _ = require('lodash');
-
-const { color: d3Color } = require('d3-color');
+import path from 'path';
+import fs from 'fs';
+import _ from 'lodash';
+import color as d3Color from 'd3-color';
 const { hsv: d3Hsv } = require('d3-hsv');
 const { exit } = require('process');
 
